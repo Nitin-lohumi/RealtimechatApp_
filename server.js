87dadb1c6b7701add7ea62af6703b7f40c86836e
Nitin-Lohumi <lohuminitin@gmail.com>
2025-02-08 19:30:42 +0530

    add

--- server.js
+++ server.js
@@ -48,6 +48,9 @@ io.on("connection", async (socket) => {
     io.emit("userCount", io.engine.clientsCount);
   });
 });
+app.get("/",(req,res)=>{
+  res.send("Server is running!");
+})
 const Port  = process.env.PORT||4000
 server.listen(Port, () => {
   console.log(`Socket.IO server running on ${Port}`);
